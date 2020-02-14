73f03d12557a666e351d30753ecdfe1bf211d1c1
Robin <phanmaison@gmail.com>
2020-02-14 15:05:12 +0700

    Add test project

---
+++ src/TestWebApp/Models/SampleModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebApp.Models
+{
+    public class SampleModel
+    {
+        [UIHint("String")]
+        public string SampleString1 { get; set; }
+        [UIHint("String")]
+        public string SampleString2 { get; set; }
+        [UIHint("String")]
+        public string SampleString3 { get; set; }
+
+
+        [UIHint("DropdownList")]
+        public string SampleDropdown1 { get; set; }
+
+    }
+}
